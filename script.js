b25ef7860aec710d1fe873af4aeb19630fb4c356
Queen of Corgis <tana.owens@gmail.com>
2020-07-01 19:18:25 -0600

    completely re-wrote questions object

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+var score = 0;
+var startBtn = document.getElementById("#start-button");
+var timeEl = document.querySelector("#timer");
+
+
 function setTime() {
-  var timeEl = document.querySelector("#timer");
+  
   var secondsLeft = 90;
   // console.log("Ok cool");
 
@@ -14,75 +19,33 @@ function setTime() {
   }, 1000);
 }
 
+
+
+
+// on click event, "this" is going to equal what is clicked; then you can pull value $(this).val();
 function startGame() {
-  var score = 0;
+  
 
   var questions = {
-    question1: ["Curly", "Square", "Angled", "Squiggly"],
+    "Question 1 - Which brackets are used to enclose an array?": ["Curly", "Square", "Angled", "Squiggly"],
 
-    question2: [
+    "Question 2 - What does CSS stand for?": [
       "Cascading Style Sheets",
       "Computer Science Styles",
       "Crashing Style Sections",
       "Computer Style Selections",
     ],
 
-    question3: ["Array", "Boolean", "Prototypes", "Method"],
+    "Question 3 - Which of the following is not an object?": ["Array", "Boolean", "Prototypes", "Method"],
 
-    question4: ["Index", "About Me", "Home", "Return"],
+    "Question 4 - Which type of page will a web browser generally seek first?": ["Index", "About Me", "Home", "Return"],
 
-    question5: false,
+    "Question 5: True or false - NULL and UNDEFINED mean the same thing.": false,
   };
 
-  $("#container").text("Question 1: Which brackets are used to enclose an array?");
-  $("#container").append("<p>", "<button>Curly</button>"); // can I assign array values to these buttons?
-  $("#container").append("<p>", "<button>Square</button>");
-  $("#container").append("<p>", "<button>Angled</button>");
-  $("#container").append("<p>","<button>Squiggly</button>");
-
-  if (question1[1]) {
-    score++;
-    alert("Correct! You get one point!");
-  } else {
-    alert("Incorrect! You lose 10 seconds!");
-    secondsLeft = -10;
-  }
-
-  $("#container").html("Question 2: What does CSS stand for?");
-  $("#container").append("<p>", "<button>Cascading Style Sheets</button>");
-  $("#container").append("<p>", "<button>Computer Science Styles</button>");
-  $("#container").append("<p>", "<button>Crashing Style Sections</button>");
-  $("#container").append("<p>", "<button>Computer Style Selections</button>");
-
-  if (question2[0]) {
-    score++;
-    alert("Correct! You get one point!");
-  } else {
-    alert("Incorrect! You lose 10 seconds!");
-    secondsLeft = -10;
-  }
-
-  $("#container").html("Question 3: Which of the following is not an object?");
-  $("#container").append("<p>", "<button>Array</button>");
-  $("#container").append("<p>", "<button>Boolean</button>");
-  $("#container").append("<p>", "<button>Prototype</button>");
-  $("#container").append("<p>", "<button>Method</button>");
-
-  if (question3[2]) {
-    score++;
-    alert("Correct! You get one point!");
-  } else {
-    alert("Incorrect! You lose 10 seconds!");
-    secondsLeft = -10;
-  }
-
-  $("#container").html("Question 4: Which type of page will a web browser generally seek first?");
-  $("#container").append("<p>", "<button>Index</button>");
-  $("#container").append("<p>", "<button>About Me</button>");
-  $("#container").append("<p>", "<button>Home</button>");
-  $("#container").append("<p>", "<button>Return</button>");
   
-  if (question4[0]) {
+
+  if (questions.question1[1]) {
     score++;
     alert("Correct! You get one point!");
   } else {
@@ -90,12 +53,38 @@ function startGame() {
     secondsLeft = -10;
   }
 
-  $("#container").html("Question 5: True or false - NULL and UNDEFINED mean the same thing.");
-  $("#container").append("<p>", "<button>True</button>");
-  $("#container").append("<p>", "<button>False</button>");
+
+
+  if (questions.question2[0]) {
+    score++;
+    alert("Correct! You get one point!");
+  } else {
+    alert("Incorrect! You lose 10 seconds!");
+    secondsLeft = -10;
+  }
+
  
 
-  if ((question5 = false)) {
+  if (questions.question3[2]) {
+    score++;
+    alert("Correct! You get one point!");
+  } else {
+    alert("Incorrect! You lose 10 seconds!");
+    secondsLeft = -10;
+  }
+
+  
+  if (questions.question4[0]) {
+    score++;
+    alert("Correct! You get one point!");
+  } else {
+    alert("Incorrect! You lose 10 seconds!");
+    secondsLeft = -10;
+  }
+
+  
+
+  if (questions.question5 = false) {
     score++;
     alert("Correct! You get one point!");
   } else {
@@ -113,4 +102,10 @@ $("#start-button").on("click", function () {
   startGame();
 });
 
-// // Question Object
+function storeHighScores () {
+  
+}
+
+function endGame () {
+  
+}
